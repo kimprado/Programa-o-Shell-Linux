c1931f25412d2fec1d56ee9095932fc911a1010b
Kim Prado <kimprado21@gmail.com>
2021-09-15 22:31:33 -0400

    Melhorias significativas nos desafios da semana 02
    Resolução otimizada dos desafios.

--- desafios/semana-2/01-desafio-semana-2.sh
+++ desafios/semana-2/01-desafio-semana-2.sh
@@ -1,7 +1,3 @@
 # Como posso listar somente o conteúdo do 26º registro do arquivo /etc/passwd?
 
-# Long version
-# grep -n -E '.*' /etc/passwd | grep '^26:' |  grep -o -E '[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*$'
-
-# Shorter version
-grep -nE '*' /etc/passwd | grep '^26:' | grep -oE '[^:]*(:[^:]*){6}$'
+grep -Fn : /etc/passwd | grep '^26:' | grep -o '[^0-9:].*'
--- desafios/semana-2/03-desafio-semana-2.sh
+++ desafios/semana-2/03-desafio-semana-2.sh
@@ -1,3 +1,3 @@
 # Quero mandar para saída somente a 22ª (vigésima segunda) linha após o usuário sync do /etc/passwd.
 
-SyncPos=$(grep -nE '^sync:' /etc/passwd | grep -oE '^[[:digit:]]+'); tail -n +$SyncPos /etc/passwd | grep -nE '*' | grep '^23:' | grep -oE '[^:]*(:[^:]*){6}$'
+grep '^sync:' /etc/passwd -A22 | tail -1
