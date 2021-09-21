24989c41d806151f923337c4c7ab05c276a29475
Kim Prado <kimprado21@gmail.com>
2021-09-21 18:34:17 -0400

    Exercícios e desafio

---
+++ desafios/semana-3/01-desafio-semana-3.sh
@@ -0,0 +1,3 @@
+# listar de 1 a 10 em duas colunas, uma com impares e outra com pares
+
+seq 10 | xargs -L2
---
+++ texts/paste-command/test-paste-command.sh
@@ -0,0 +1,10 @@
+cat impar 
+paste impar 
+paste -s impar 
+paste -s impar par 
+paste -s -d+ impar 
+paste -s -d+ impar | bc
+paste -s -d ABCD impar
+paste -s -d + impar
+paste -s -d +a impar
+paste -s -d +az impar
