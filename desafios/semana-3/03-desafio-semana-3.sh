d1b760e32b73a82279006c5db31288e526fcfc4a
Kim Prado <kimprado21@gmail.com>
2021-09-21 21:10:57 -0400

    Melhorias semana 03

--- desafios/semana-3/03-desafio-semana-3.sh
+++ desafios/semana-3/03-desafio-semana-3.sh
@@ -1,3 +1,6 @@
 # Dado "seq -w 30 | paste -d: - - - - -" , que comando devo executar para que a saída seja  "01:02:03=04=05"
 
-seq -w 30 | paste -d: - - - - - | sed -E 's|:([0-9]{2}):([0-9]{2})$|=\1=\2|'
+# Long version
+# seq -w 30 | paste -d: - - - - - | sed -E 's|:([0-9]{2}):([0-9]{2})$|=\1=\2|'
+
+seq -w 30 | paste -d: - - - - - | sed  's|:|=|3g'
