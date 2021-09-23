25a81c4fcbd3f8fe5cc8cb7a1ee4f2d928c2295d
Kim Prado <kimprado21@gmail.com>
2021-09-22 20:31:10 -0400

    Diferença de performance

--- desafios/semana-3/02-desafio-semana-3.sh
+++ desafios/semana-3/02-desafio-semana-3.sh
@@ -1,3 +1,15 @@
 # cabeçalho "I P" (Impares Pares) antes dos dados
 
 cat - <<< "I P" nums
+
+time for ((i=0; i<200; i++))
+{
+cat <<< "I P
+$(cat  nums)
+" > /dev/null
+}
+
+time for ((i=0; i<200; i++))
+{
+cat - <<< "I P" nums 
+} > /dev/null
